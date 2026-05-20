9045a7d18f72ba0b684443e9481d0b880ed23f5b
Advait Sankaran <advait.sankaran2006@gmail.com>
2026-05-19 17:31:47 -0700

    Added data augmentations and dataloaders for train/test/val

---
+++ mushroom_model.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from PIL import Image
+from torchvision import datasets
+from torchvision.transforms import v2
+import splitfolders
+
+torch.manual_seed(127)
+
+train_transforms = v2.Compose([
+    v2.ToTensor(), # Applied to all images
+    v2.Resize(size=(100, 100)), # Applies to all images
+    v2.RandomHorizontalFlip(0.15), # Randomly applied to images with 0.15 probability
+    v2.RandomPerspective(0.3, 0.15), # Randomly applied distortion to images with 0.15 probability
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Normalizes all inputs
+    v2.ToPILImage() # Converts data back to images
+])
+
+val_test_transforms = v2.Compose([
+    v2.ToTensor(),
+    v2.Resize(size=(100, 100)),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    v2.ToPILImage()
+])
+
+splitfolders.ratio('organized_mushroom_data', output='split_mushroom_data', seed=1337, ratio=(.8, .1, .1))
+
+train_dataset = datasets.ImageFolder('split_mushroom_data/train', transform=train_transforms)
+val_dataset = datasets.ImageFolder('split_mushroom_data/val', transforms=val_test_transforms)
+test_dataset = datasets.ImageFolder('split_mushroom_data/test', transforms=val_test_transforms)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+NUM_EPOCHS = 10
+# Will make loop traversing through the loaders and printing intput and output values later
