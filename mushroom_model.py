f264be752e5ebaa5b2cbd5123b727e438456ddee
Leis873 <chailey7887@gmail.com>
2026-05-31 02:40:27 -0700

    added confusion matrix, precision, recall

--- mushroom_model.py
+++ mushroom_model.py
@@ -10,6 +10,7 @@ from torchvision.transforms import v2
 import splitfolders
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score
 
 torch.manual_seed(127)
 
@@ -84,7 +85,7 @@ model.train()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-NUM_EPOCHS = 20
+NUM_EPOCHS = 1
 
 for images, labels in train_loader:
     break
@@ -100,7 +101,7 @@ for i in range(NUM_EPOCHS):
         optimizer.step()
         class_train_preds = torch.max(train_preds, axis=1)[1]
         total_correct += (class_train_preds == train_y).sum().item()
-    print(f"Training Accuracy: {total_correct / len(train_dataset)}")
+    print(f"Epoch: {i + 1} \nTraining Accuracy: {total_correct / len(train_dataset)}")
     #print loss
     total_correct = 0
     for idx, (val_X, val_y) in enumerate(val_loader):
@@ -111,6 +112,8 @@ for i in range(NUM_EPOCHS):
     print(f"Validation Accuracy: {total_correct / len(val_dataset)}")
 
 model.eval()
+total_targets = []
+total_preds = []
 with torch.no_grad():
     total_correct = 0
     for idx, (test_X, test_y) in enumerate(test_loader):
@@ -119,4 +122,19 @@ with torch.no_grad():
         class_test_preds = torch.max(test_preds, axis=1)[1]
         total_correct += (class_test_preds == test_y).sum().item()
 
+        total_targets.extend(test_y.tolist())
+        total_preds.extend(class_test_preds.squeeze().tolist())
+
     print(f"Test Accuracy: {total_correct / len(test_dataset)}")
+
+# Calculate precision, recall, and create Confusion Matrix
+precision = precision_score(total_targets, total_preds, average=None)
+recall = recall_score(total_targets, total_preds, average=None)
+print(f"Precision: Conditionally Edible - {precision[0]}, Deadly - {precision[1]}, Edible - {precision[2]}, Poisonous - {precision[3]}")
+print(f"Recall: Conditionally Edible - {recall[0]}, Deadly - {recall[1]}, Edible - {recall[2]}, Poisonous - {recall[3]}")
+
+cm = confusion_matrix(total_targets, total_preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=test_dataset.classes)
+disp.plot()
+plt.show()
+
