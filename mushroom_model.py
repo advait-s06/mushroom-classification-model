4026ff5c7bdd4e6ef4d8f6869420015feca25144
Advait Sankaran <advait.sankaran2006@gmail.com>
2026-05-31 14:05:02 -0700

    finished train/val loss graphs

--- mushroom_model.py
+++ mushroom_model.py
@@ -18,7 +18,7 @@ train_transforms = v2.Compose([
     v2.ToTensor(), # Applied to all images
     v2.Resize(size=(100, 100)), # Applies to all images
     v2.RandomHorizontalFlip(0.15), # Randomly applied to images with 0.15 probability
-    # v2.RandomPerspective(0.3, 0.15), # Randomly applied distortion to images with 0.15 probability
+    v2.RandomPerspective(0.3, 0.15), # Randomly applied distortion to images with 0.15 probability
     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Normalizes all inputs
 ])
 
@@ -39,20 +39,20 @@ val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
 # Visualization
-# for idx, (images, labels) in enumerate(train_loader):
-    # break
+for idx, (images, labels) in enumerate(train_loader):
+    break
 
-# plt.figure(figsize=(20,20))
+plt.figure(figsize=(20,20))
 
-# for idx, image in enumerate(images):
-    # if idx < 100:
-       # plt1 = plt.subplot(10, 10, idx + 1)
-       # image = image.permute(1, 2, 0)
-       # plt1.imshow(image)
-        #plt1.set_title(train_dataset.classes[labels[idx].item()])
-        #plt1.axis('off')
-#plt.tight_layout()
-#plt.show()
+for idx, image in enumerate(images):
+    if idx < 100:
+        plt1 = plt.subplot(10, 10, idx + 1)
+        image = image.permute(1, 2, 0)
+        plt1.imshow(image)
+        plt1.set_title(train_dataset.classes[labels[idx].item()])
+        plt1.axis('off')
+plt.tight_layout()
+plt.show()
 
 class MyModel(nn.Module):
     def __init__(self):
@@ -85,12 +85,14 @@ model.train()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-NUM_EPOCHS = 1
+NUM_EPOCHS = 5
 
 for images, labels in train_loader:
     break
 
 # Iterating through training, val, and test data
+train_losses = []
+val_losses = []
 for i in range(NUM_EPOCHS):
     total_correct = 0
     for idx, (train_X, train_y) in enumerate(train_loader):
@@ -101,19 +103,34 @@ for i in range(NUM_EPOCHS):
         optimizer.step()
         class_train_preds = torch.max(train_preds, axis=1)[1]
         total_correct += (class_train_preds == train_y).sum().item()
-    print(f"Epoch: {i + 1} \nTraining Accuracy: {total_correct / len(train_dataset)}")
-    #print loss
+    train_losses.append(loss.detach().numpy())
+    print(f"Epoch: {i + 1} \nTraining Accuracy: {total_correct / len(train_dataset)} and Loss: {loss}")
     total_correct = 0
     for idx, (val_X, val_y) in enumerate(val_loader):
         val_preds = model(val_X)
         loss = criterion(val_preds, val_y)
         class_val_preds = torch.max(val_preds, axis=1)[1]
         total_correct += (class_val_preds == val_y).sum().item()
-    print(f"Validation Accuracy: {total_correct / len(val_dataset)}")
+    val_losses.append(loss.detach().numpy())
+    print(f"Validation Accuracy: {total_correct / len(val_dataset)} and Loss: {loss}")
+
+# train/val loss graph
+plt.title('Train Loss vs. Epochs')
+plt.plot(range(NUM_EPOCHS), train_losses)
+plt.xlabel("Epoch #")
+plt.ylabel("Train Loss")
+plt.show()
+
+plt.title('Val Loss vs. Epochs')
+plt.plot(range(NUM_EPOCHS), val_losses)
+plt.xlabel("Epoch #")
+plt.ylabel("Val Loss")
+plt.show()
 
 model.eval()
 total_targets = []
 total_preds = []
+test_losses = []
 with torch.no_grad():
     total_correct = 0
     for idx, (test_X, test_y) in enumerate(test_loader):
@@ -124,8 +141,9 @@ with torch.no_grad():
 
         total_targets.extend(test_y.tolist())
         total_preds.extend(class_test_preds.squeeze().tolist())
+    test_losses.append(loss.detach().numpy())
 
-    print(f"Test Accuracy: {total_correct / len(test_dataset)}")
+    print(f"Test Accuracy: {total_correct / len(test_dataset)} and Loss: {loss}")
 
 # Calculate precision, recall, and create Confusion Matrix
 precision = precision_score(total_targets, total_preds, average=None)
