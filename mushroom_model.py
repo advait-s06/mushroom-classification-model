1480154a501684356fe7c7a0af27983ee81b27b1
Advait Sankaran <advait.sankaran2006@gmail.com>
2026-06-07 22:58:20 -0700

    girl???

--- mushroom_model.py
+++ mushroom_model.py
@@ -11,7 +11,7 @@ import splitfolders
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score
-import torch.optim.lr_scheduler as ExpLR
+from torch.optim.lr_scheduler import ExponentialLR as ExpLR
 
 torch.manual_seed(127)
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -20,7 +20,6 @@ train_transforms = v2.Compose([
     v2.ToTensor(), # Applied to all images
     v2.Resize(size=(100, 100)), # Applies to all images
     v2.RandomHorizontalFlip(0.15), # Randomly applied to images with 0.15 probability
-    v2.RandomPerspective(0.3, 0.15), # Randomly applied distortion to images with 0.15 probability
     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Normalizes all inputs
 ])
 
@@ -36,9 +35,9 @@ train_dataset = ImageFolder('split_mushroom_data/train', transform=train_transfo
 val_dataset = ImageFolder('split_mushroom_data/val', transform=val_test_transforms)
 test_dataset = ImageFolder('split_mushroom_data/test', transform=val_test_transforms)
 
-train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=16, pin_memory=True)
+val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False, num_workers=16, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=16, pin_memory=True)
 
 # Visualization
 visualize = input("Do you want to visualize the images (y/n)? ")
@@ -62,26 +61,30 @@ class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 10, 3, 1, 1)
-        self.batchNorm1 = nn.BatchNorm2d(10)
+        # self.batchNorm1 = nn.BatchNorm2d(10)
         self.conv2 = nn.Conv2d(10, 20, 3, 1, 1)
-        self.batchNorm2 = nn.BatchNorm2d(20)
+        # self.batchNorm2 = nn.BatchNorm2d(20)
         self.conv3 = nn.Conv2d(20, 30, 5, 1, 2)
-        self.batchNorm3 = nn.BatchNorm2d(30)
+        # self.batchNorm3 = nn.BatchNorm2d(30)
         self.conv4 = nn.Conv2d(30, 35, 5, 1, 2)
-        self.batchNorm4 = nn.BatchNorm2d(35)
+        # self.batchNorm4 = nn.BatchNorm2d(35)
         self.linear1 = nn.Linear(35*6*6, 300)
         self.linear2 = nn.Linear(300, 4)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
 
     def forward(self, input):
-        x = self.relu(self.batchNorm1(self.conv1(input)))
+        # x = self.relu(self.batchNorm1(self.conv1(input)))
+        x = self.relu(self.conv1(input))
         x = self.pool(x)
-        x = self.relu(self.batchNorm2(self.conv2(input)))
+        # x = self.relu(self.batchNorm2(self.conv2(x)))
+        x = self.relu(self.conv2(x))
         x = self.pool(x)
-        x = self.relu(self.batchNorm3(self.conv3(input)))
+        # x = self.relu(self.batchNorm3(self.conv3(x)))
+        x = self.relu(self.conv3(x))
         x = self.pool(x)
-        x = self.relu(self.batchNorm4(self.conv4(input)))
+        # x = self.relu(self.batchNorm4(self.conv4(x)))
+        x = self.relu(self.conv4(x))
         x = self.pool(x)
         x = x.flatten(start_dim=1)
         x = self.relu(self.linear1(x))
@@ -92,10 +95,11 @@ model = MyModel()
 model = model.to(device)
 model.train()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.01)
-scheduler = ExpLR(optimizer, gamma=0.9)
-NUM_EPOCHS = 20
+class_weights = torch.tensor([1.2, 1.7, 1.0, 1.5])
+criterion = nn.CrossEntropyLoss(weight=class_weights.to(device))
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.01)
+# scheduler = ExpLR(optimizer, gamma=0.9)
+NUM_EPOCHS = 25
 
 for images, labels in train_loader:
     break
@@ -116,7 +120,7 @@ if __name__ == "__main__":
             optimizer.step()
             class_train_preds = torch.max(train_preds, axis=1)[1]
             total_correct += (class_train_preds == train_y).sum().item()
-        scheduler.step()
+        # scheduler.step()
         train_losses.append(loss.detach().cpu().numpy())
         print(f"Epoch: {i + 1} \nTraining Accuracy: {total_correct / len(train_dataset)} and Loss: {loss}")
         total_correct = 0
